a61fbca0a8ba44a235f488c746430df3a60b612f
Matt Chudoba <mchudoba5@gmail.com>
2015-01-20 20:52:51 -0500

    Camera movement

--- Batman_Alpha/Assets/__Scripts/FollowCam.cs
+++ Batman_Alpha/Assets/__Scripts/FollowCam.cs
@@ -7,19 +7,37 @@ public class FollowCam : MonoBehaviour
 	public GameObject		batman;
 	public float			minY = 4f;
 	public float			minX = 2.9f;
+	public float			maxX = 113f;
+	public float			upperBound = 3f;
+	public float			lowerBound = -2f;
 
 	void Update()
 	{
 		float batman_x = batman.transform.position.x;
 		float batman_y = batman.transform.position.y;
+		float cam_x = batman_x;
+		float cam_y = transform.position.y;
 
-		if (batman_y < minY)
-			batman_y = minY;
+		PE_Obj batmanPeo = batman.GetComponent<PE_Obj>();
+		float cam_y_delta = Mathf.Abs(batmanPeo.pos1.y - batmanPeo.pos0.y);
+		float batman_delta = batman.transform.lossyScale.y / 2f;
 
-		if (batman_x < minX)
-			batman_x = minX;
+		if (batman_y - cam_y + batman_delta > upperBound)
+			cam_y += cam_y_delta;
 
-		this.transform.position = new Vector3(batman_x, batman_y, -10f);
+		if (batman_y - cam_y - batman_delta < lowerBound)
+			cam_y -= cam_y_delta;
+
+		if (cam_y < minY)
+			cam_y = minY;
+
+		if (cam_x < minX)
+			cam_x = minX;
+
+		if (cam_x > maxX)
+			cam_x = maxX;
+
+		transform.position = new Vector3(cam_x, cam_y, -10f);
 	}
 
 }
