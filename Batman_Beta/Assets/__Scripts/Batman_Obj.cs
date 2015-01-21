9871ebfa849063b754653ad7613a62e62923feb3
Matt Chudoba <mchudoba5@gmail.com>
2015-01-20 23:26:22 -0500

    Beta first commit

---
+++ Batman_Beta/Assets/__Scripts/Batman_Obj.cs
@@ -0,0 +1,191 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Batman_Obj : MonoBehaviour
+{
+
+	private PE_Obj		thisPeo;
+
+	public Vector3		vel; // Local velocity of Batman
+	public GameObject	fist; // Reference to Batman's fist GameObject
+	public float		minJumpVel = 5f;
+	public float		maxJumpVel = 10f;
+	public float		jumpRateIncrease = 0.1f;
+	public float		h_speed = 6f; // Horizontal walking speed
+	public float		duck = 0.66f; // Percentage to shrink Batman to duck
+	public bool			atMaxJump = false;
+	public bool			isJumping = false;
+	public bool			grounded = false; // True if Batman is on the ground
+	public bool			isDucked = false; // True if Batman is currently ducking
+	public float		attackTimer = 0; // Timer for running an attack animation
+	public float		attackTimerVal = 0.5f;
+	public float		jumpTimer = 0;
+	public float		jumpTimerVal = 0.1f;
+	public float		xVelBeforeJump = 0;
+
+	void Start ()
+	{
+		thisPeo = GetComponent<PE_Obj>();
+	}
+		
+	void Update()
+	{
+		vel = thisPeo.vel;
+		grounded = (thisPeo.ground != null);
+
+		if (attackTimer > 0)
+			attackTimer -= Time.deltaTime;
+
+		if (attackTimer <= 0)
+			Move();
+
+		if (jumpTimer > 0)
+		{
+			jumpTimer -= Time.deltaTime;
+			vel.x = 0;
+		}
+
+		Jump();
+		Duck();
+		Punch();
+
+		thisPeo.vel = vel;
+	}
+
+	void Move()
+	{
+		float vX = Input.GetAxis("Horizontal");
+
+		if (grounded || (vX * vel.x > 0 && xVelBeforeJump * vel.x >= 0)) vel.x = vX * h_speed;
+		else if (vX * vel.x < 0 && Mathf.Abs(vel.x) > 1f) {
+			if(vel.x > 0) vel.x -= 0.15f;
+			else if (vel.x < 0) vel.x += 0.15f;
+		}
+		else if (Mathf.Abs(vel.x) > h_speed / 2f) vel.x /= 1.2f;
+		else if (xVelBeforeJump == 0) vel.x = vX * h_speed / 3f;
+
+		// Change direction
+		Quaternion rot = transform.rotation;
+		if (vX > 0)
+		{
+			rot.y = 0;
+			transform.rotation = rot;
+		}
+		else if (vX < 0)
+		{
+			rot.y = 180f;
+			transform.rotation = rot;
+		}
+	}
+
+	void Jump()
+	{
+		if ((Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Period))
+		    && grounded)
+		{
+			xVelBeforeJump = vel.x;
+			vel.x = 0;
+			jumpTimer = jumpTimerVal;
+			isJumping = true;
+			atMaxJump = true;
+
+			Vector3 scale = transform.localScale;
+			scale.y *= duck;
+			transform.localScale = scale;
+			
+			// Move location back to ground
+			Vector3 pos = transform.position;
+			pos.y -= duck / 2f;
+			transform.position = pos;
+
+			return;
+		}
+
+		if (grounded && jumpTimer <= 0 && isJumping)
+		{
+			vel.x = xVelBeforeJump;
+			vel.y = minJumpVel;
+			thisPeo.ground = null; // Jumping will set ground = null
+			atMaxJump = false;
+			isJumping = false;
+
+			Vector3 scale = transform.localScale;
+			scale.y /= duck;
+			transform.localScale = scale;
+			
+			// Move location back to ground
+			Vector3 pos = transform.position;
+			pos.y += duck / 2f;
+			transform.position = pos;
+		}
+
+		if (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.Period))
+		{
+			if (vel.y < maxJumpVel && !atMaxJump)
+			{
+				vel.y += jumpRateIncrease;
+			}
+			else
+			{
+				atMaxJump = true;
+			}
+		}
+
+		if (Input.GetKeyUp(KeyCode.X) || Input.GetKeyUp(KeyCode.Period))
+		{
+			atMaxJump = true;
+
+			if (jumpTimer <= 0)
+				isJumping = false;
+		}
+	}
+	
+	void Duck()
+	{
+		Vector3 scale = transform.localScale;
+		
+		if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+		    && thisPeo.vel == Vector3.zero && !isDucked)
+		{
+			isDucked = true;
+
+			scale.y *= duck;
+			transform.localScale = scale;
+			
+			// Move location back to ground
+			Vector3 pos = transform.position;
+			pos.y -= duck / 2f;
+			transform.position = pos;
+		}
+		if (((Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+		     || thisPeo.vel != Vector3.zero) && isDucked)
+		{
+			isDucked = false;
+			
+			scale.y /= duck;
+			transform.localScale = scale;
+			
+			// Move location back to ground
+			Vector3 pos = transform.position;
+			pos.y += duck / 2f;
+			transform.position = pos;
+		}
+	}
+
+	void Punch()
+	{
+		if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Comma))
+		{
+			fist.renderer.enabled = true;
+			fist.collider.enabled = true;
+			attackTimer = attackTimerVal;
+			if (grounded)
+				vel.x = 0;
+		}
+		else if (attackTimer <= 0)
+		{
+			fist.renderer.enabled = false;
+			fist.collider.enabled = false;
+		}
+	}
+}
---
+++ Batman_Beta/Assets/__Scripts/PhysEngine.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum PE_Dir // The direction in which the PE_Obj is moving
+{
+	still,
+	up,
+	down,
+	upRight,
+	downRight,
+	downLeft,
+	upLeft
+}
+
+public class PhysEngine : MonoBehaviour
+{
+
+	static public List<PE_Obj>	objs;
+
+	public Vector3		gravity = new Vector3(0, -9.8f, 0);
+
+	void Awake()
+	{
+		objs = new List<PE_Obj>();
+	}
+
+	void FixedUpdate()
+	{
+		// Handle the timestep for each object
+		float dt = Time.fixedDeltaTime;
+		foreach (PE_Obj current in objs)
+			TimeStep(current, dt);
+
+		// Finalize positions and rotations
+		foreach (PE_Obj current in objs)
+		{
+			current.transform.position = current.pos1;
+		}
+	}
+
+	public void TimeStep(PE_Obj current, float dt)
+	{
+		if (current.still)
+		{
+			current.pos0 = current.pos1 = current.transform.position;
+			return;
+		}
+
+		// Velocity
+		Vector3 tAcc = current.acc;
+		tAcc += gravity;
+		current.vel += tAcc * dt;
+
+		// Determine direction of current object
+		if (current.vel.x == 0)
+		{
+			if (current.vel.y > 0)
+				current.dir = PE_Dir.up;
+			else
+				current.dir = PE_Dir.down;
+		}
+		else if (current.vel.x>0 && current.vel.y>0)
+		{
+			current.dir = PE_Dir.upRight;
+		}
+		else if (current.vel.x>0 && current.vel.y<=0)
+		{
+			current.dir = PE_Dir.downRight;
+		}
+		else if (current.vel.x<0 && current.vel.y<=0)
+		{
+			current.dir = PE_Dir.downLeft;
+		}
+		else if (current.vel.x<0 && current.vel.y>0)
+		{
+			current.dir = PE_Dir.upLeft;
+		}
+
+		// Position
+		current.pos1 = current.pos0 = current.transform.position;
+		current.pos1 += current.vel * dt;
+	}
+
+}
